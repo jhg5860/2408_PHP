80765c09b01a5fbce3bd8248d1338dabed8075ab
jhg5860 <jaehoon6521@naver.com>
2024-11-01 00:35:08 +0900

    24.11.01 home

--- JavaScript/Tng/41.js
+++ JavaScript/Tng/41.js
@@ -1,15 +1,24 @@
 (()=> {
-
+    let start;
+const BTN_STOP = document.querySelector('#btn_stop');
+const BTN_RESTART = document.querySelector('#btn_restart');
+const CLOCK = document.querySelector('#clock');
+BTN_STOP.addEventListener('click',()=> {
+    clearInterval(start);    
+})
+BTN_RESTART.addEventListener('click', ()=> {
+   // 즉시 시간 업데이트 
+    clock(); 
+    // 타이머 시작
+    start = setInterval(clock, 1000);     
+})
 console.log(timer());
-
-})();
-
-
 function timer (){    
     clock();
-    setInterval(clock , 1000);
+    start =setInterval(clock , 1000);
+   
 }
-
+})();
 
 function clock() {
 const addlpadZero = (num , length) => {
@@ -21,5 +30,23 @@ const HOUR = addlpadZero(NOW.getHours(), 2);
 const MINUTES = addlpadZero(NOW.getMinutes() , 2);
 const SECONDS = addlpadZero(NOW.getSeconds() ,2);
 
-CLOCK.innerHTML = ` ${HOUR}:${MINUTES}:${SECONDS}`;
+// 오전 오후 표시
+    let ampm;
+    if (HOUR < 12) {
+        ampm = '오전';
+    } else {
+        ampm = '오후';
+    }  
+    
+// 24시간 
+    let half_hour;
+    if(HOUR === 0 ) {
+        half_hour =12;    
+    } else if (HOUR >12) {
+        half_hour =HOUR -12;
+    } else {
+        half_hour =HOUR;
+    }
+    CLOCK.innerHTML = `현재 시간 ${ampm} ${addlpadZero(half_hour ,2)}:${MINUTES}:${SECONDS}`;
 }
+
